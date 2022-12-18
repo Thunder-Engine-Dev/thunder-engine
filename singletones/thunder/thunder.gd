8dbfaf49e5b5f2015c57b6d3a5aa3904a12a7498
电童家的Fuaisam <2733679597@qq.com>
2022-12-18 23:59:47 +0800

    Updated the structure of singleton

--- singletones/thunder/thunder.gd
+++ singletones/thunder/thunder.gd
@@ -2,7 +2,15 @@
 
 extends Node
 
-var _current_frame: Frame # Reference to the current frame scene
+const DEFAULT_DELTA:float = 0.0001 # Default delta
+
+var _current_frame: Frame: # Reference to the current frame scene
+	set(frame):
+		assert(is_instance_valid(node) || !(node is Frame), "Frame node is invalid")
+		_current_frame = frame
+	get:
+		assert(is_instance_valid(_current_frame) || !(_current_frame is Frame), "Frame node is invalid or not set")
+		return _current_frame
 var _current_camera: Camera2D # Reference to the current camera node
 var _current_player: Player # Reference to the current player
 
@@ -13,14 +21,14 @@ func get_or_null(obj: Variant, key: String):
 
 
 func get_delta(delta: float) -> float:
-	return 50 / (1 / (delta if not delta == 0 else 0.0001))
+	return 50 * (delta if delta != 0 else DEFAULT_DELTA)
 
 
-func set_current_frame(node: Frame) -> void:
-	assert(is_instance_valid(node) || !(node is Frame), "Frame node is invalid")
-	_current_frame = node
+#func set_current_frame(node: Frame) -> void:
+#	assert(is_instance_valid(node) || !(node is Frame), "Frame node is invalid")
+#	_current_frame = node
 
 
-func get_current_frame() -> Frame:
-	assert(is_instance_valid(_current_frame) || !(_current_frame is Frame), "Frame node is invalid or not set")
-	return _current_frame
+#func get_current_frame() -> Frame:
+#	assert(is_instance_valid(_current_frame) || !(_current_frame is Frame), "Frame node is invalid or not set")
+#	return _current_frame
