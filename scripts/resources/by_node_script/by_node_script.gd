9a5d5588c9f8395d4098d1d41b7f9f3f575010f0
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-02-10 19:57:12 +0800

    extended by_node_script for better usage

--- scripts/resources/by_node_script/by_node_script.gd
+++ scripts/resources/by_node_script/by_node_script.gd
@@ -9,23 +9,30 @@ class_name ByNodeScript
 signal extra_script # Only used as an identifier, no other usage!
 
 var node: Node
+var vars: Dictionary
+var other_nodes: Dictionary
+var other_resources: Dictionary
 
 
-static func activate_script(script: GDScript, by: Node) -> GDScript:
+static func activate_script(script: GDScript, by: Node, new_vars: Dictionary = {}, new_other_nodes: Dictionary = {}, new_other_resources: Dictionary = {}) -> GDScript:
 	var result: GDScript
 	if script:
 		if script.has_script_signal(&"extra_script"):
-			result = script.new(by)
+			result = script.new(by, new_vars, new_other_nodes, new_other_resources)
 		else:
 			push_error("[Extra Script Error] Inserted script is not extended from ByNodeScript. Please check it")
 	return result
 
 
-func _init(by: Node) -> void:
+func _init(by: Node, new_vars: Dictionary = {}, new_other_nodes: Dictionary = {}, new_other_resources: Dictionary = {}) -> void:
 	if !by: return
 	node = by
+	vars = new_vars.duplicate(true)
+	other_nodes = new_other_nodes.duplicate(true)
+	other_resources = new_other_resources.duplicate(true)
 	_ready()
 	node.tree_entered.connect(_enter_tree)
+	node.tree_exited.connect(_exit_tree)
 	node.get_tree().process_frame.connect(_process.bind(node.get_process_delta_time()))
 	node.get_tree().physics_frame.connect(_physics_process.bind(node.get_physics_process_delta_time()))
 
