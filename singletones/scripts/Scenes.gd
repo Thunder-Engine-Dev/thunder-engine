c5b4496eae4d7610733192f3421362bb5cfe6992
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-02-11 11:57:27 +0800

    optimized Scenes singleton

--- singletones/scripts/Scenes.gd
+++ singletones/scripts/Scenes.gd
@@ -1,22 +1,29 @@
 extends Node
 
 var current_scene:Node
+var current_scene_packed:PackedScene
 var current_root:Node
-var buffered_scenes:Array[Node]
 
-@onready var root:Node = get_tree().root
+var hung_scene:Node
+var hung_scene_process:bool
+var hung_scene_physics_process:bool
 
 signal scene_changed(to:PackedScene)
+signal scene_got_hung(hung:Node)
 
 
 # Register scene
 func register(scene:Node) -> void:
 	current_scene = scene
 	if current_scene.is_inside_tree(): current_root = scene.get_parent()
+	
+	current_scene_packed = PackedScene.new()
+	current_scene_packed.pack(current_scene)
 
 
 # Install scene
 func install_scene() -> void:
+	if current_root.is_ancestor_of(current_scene): return
 	current_root.add_child(current_scene)
 
 
@@ -25,61 +32,86 @@ func load_scene(scene:Node) -> void:
 	register(scene)
 	install_scene()
 
-func load_scene_from_buffer(buffer_id:int) -> void:
-	register(get_buffered_scene(buffer_id))
-	install_scene()
-
 func load_scene_from_packed(pck:PackedScene) -> void:
 	if !pck: return
 	var scene:Node = pck.instantiate()
 	load_scene(scene)
 
-func load_scene_from_path(path:String) -> void:
-	if path.is_empty(): return
-	load_scene_from_packed(load(path) as PackedScene)
-
 
 # Unload scene
-func unload_current_scene() -> void:
+func unload_current_scene(delete_current_packed:bool = true) -> void:
+	if delete_current_packed: current_scene_packed = null
 	current_scene.queue_free()
-
-func unload_current_scene_as_buffer(buffer_id:int) -> void:
-	buffer_scene(current_scene,buffer_id)
-	current_scene.get_parent().remove_child(current_scene)
+	Thunder.stage_changed.emit()
 
 
 # Switch to scene
-func switch_to_scene(to:Node) -> void:
-	unload_current_scene()
+func switch_to_scene(to:Node,hang_current:bool = false) -> void:
+	if hang_current: hang_current_scene()
+	else: unload_current_scene()
 	load_scene(to)
+	scene_changed.emit(current_scene)
 
-func switch_to_scene_packed(pck:PackedScene) -> void:
-	unload_current_scene()
+func switch_to_scene_packed(pck:PackedScene,hang_current:bool = false) -> void:
+	if hang_current: hang_current_scene()
+	else: unload_current_scene()
 	load_scene_from_packed(pck)
+	scene_changed.emit(current_scene)
 
 
 # Reload current scene
 func reload_current_scene() -> void:
-	var path:String = current_scene.scene_file_path
-	unload_current_scene()
-	load_scene_from_path(path)
+	unload_current_scene(false)
+	load_scene_from_packed(current_scene_packed)
 
 
-# Buffer scene
-func buffer_scene(scene:Node,id:int) -> void:
-	if id < 0: return
-	if buffered_scenes.size() > id + 1: buffered_scenes.resize(id + 1)
-	buffered_scenes[id] = scene
+# Hang scene
+func hang_scene(scene:Node) -> void:
+	if hung_scene: return
+	
+	hung_scene = scene
+	
+	scene_got_hung.emit(scene)
+	
+	hung_scene_process = scene.is_processing()
+	hung_scene_physics_process = scene.is_physics_processing()
+	
+	if &"visible" in scene: scene.visible = false
+	scene.set_process(false)
+	scene.set_physics_process(false)
+	
+	Thunder.stage_changed.emit()
 
-# Clear buffered scene
-func clear_buffered_scene(id:int) -> void:
-	if abs(id + 1) > buffered_scenes.size(): return
-	buffered_scenes[id] = null
+func hang_current_scene() -> void:
+	hang_scene(current_scene)
 
-func clear_all_buffered_scenes() -> void:
-	buffered_scenes.clear()
+# Unhang scene
+func unhang_scene_as_current() -> void:
+	if !hung_scene: return
+	
+	register(hung_scene)
+	
+	scene_changed.emit(current_scene)
+	
+	if &"visible" in current_scene: current_scene.visible = true
+	current_scene.set_process(hung_scene_process)
+	current_scene.set_physics_process(hung_scene_physics_process)
+	
+	hung_scene = null
+	hung_scene_process = false
+	hung_scene_physics_process = false
+	
+	Thunder.stage_changed.emit()
 
-# Get buffered scene 
-func get_buffered_scene(id:int) -> Node:
-	if buffered_scenes.size() > id + 1: return null
-	return buffered_scenes[id]
+func unhang_scene_and_queue_free() -> void:
+	if !hung_scene: return
+	hung_scene.queue_free()
+	hung_scene = null
+
+# Swap hung scene and current scene
+func swap_hung_scene_and_current_scene() -> void:
+	if !hung_scene || !current_scene: return
+	
+	var swamped:Node = current_scene
+	unhang_scene_as_current()
+	hang_scene(swamped)
