0635dbc0eba1d5bb07981f877fd3fd61b2de442c
reflexguru <trianglestudioad@gmail.com>
2023-03-12 17:52:29 +0300

    add turn_sprite property to general enemy movement

--- scripts/nodes/general_movement/general_movement.gd
+++ scripts/nodes/general_movement/general_movement.gd
@@ -3,8 +3,11 @@ class_name GeneralMovementBody2D
 
 @export_category("GeneralMovement")
 @export var look_at_player: bool
+@export var turn_sprite: bool = true
 @export var deep_snap: bool = true
 @export var kinematic_movement: bool = true
+@export_category("References")
+@export var sprite: NodePath
 
 
 func _ready() -> void:
@@ -15,3 +18,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	motion_process(Thunder.get_delta(delta), deep_snap, kinematic_movement)
+	
+	var sprite_node = get_node_or_null(sprite)
+	if turn_sprite && sprite_node:
+		sprite_node.flip_h = speed.x < 0
