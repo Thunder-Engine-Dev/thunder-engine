98560f54e26ab57e1e27c933265b3490bd62eadd
jue131 <83157668+jue131@users.noreply.github.com>
2023-03-16 20:01:03 +0900

    fix poison mushroom error

--- objects/powerups/poisonous_mushroom/poisonous_mushroom.gd
+++ objects/powerups/poisonous_mushroom/poisonous_mushroom.gd
@@ -1,10 +1,13 @@
 extends Powerup
 
+const explosion_effect = preload("res://engine/objects/effects/explosion/explosion.tscn")
+
 func collect() -> void:
 	var player = Thunder._current_player
 	
 	if player.states.invincible_timer > 0: return
 	player.kill()
 	
-	ExplosionEffect.new(position)
+	NodeCreator.prepare_2d(explosion_effect, self).create_2d().bind_global_transform()
 	queue_free()
+
