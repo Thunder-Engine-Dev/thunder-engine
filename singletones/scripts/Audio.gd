463cf6218a84e6191aa2c514469bc48642a1e76e
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-04-09 22:23:28 +0800

    optimized doc of Audio singleton

--- singletones/scripts/Audio.gd
+++ singletones/scripts/Audio.gd
@@ -1,9 +1,10 @@
 extends Node
 
 ## Singleton that manages musics and sounds for your game[br]
-## We [color=yellow][b][i]extremely recommend[/i][/b][/color] to use the methods in the singleton for playing your musics and sounds.[br]
-## [color=orange][b]Note:[/b][/color] there are some methods with param [code]other_keys[/code]([Dictionary]), so here is the list of [color=purple]spell_keys[/color]:[br]
-## ([float])[code]pitch[/code]: Determines the pitch of the sound or music
+##
+## We [b][i]extremely recommend[/i][/b] to use the methods in the singleton for playing your musics and sounds.[br]
+## [b]Note:[/b] there are some methods with param [param other_keys]([Dictionary]), so here is the list of [i][u]spell_keys[/u][/i]:[br]
+## ([float])[param pitch]: Determines the pitch of the sound or music
 
 ## Param set when you call [method fade_music_1d_player]
 enum FadingMethod {
@@ -60,12 +61,12 @@ func _fading(delta: float) -> void:
 				fading_music_player.stop()
 			continue
 
-## Play a [color=green]sound[/color] with given [AudioStream] resource and bind the sound player to a [Node2D][br]
-## [color=orange][b]Note:[/b][/color] This method creates [AudioStreamPlayer2D] which plays sound with pan changed according to its position to the center of screen, rather than [AudioStreamPlayer].[br]
-## [code]resource[/code] is the sound stream you are going to install[br]
-## [code]ref[/code] is the node to be bound[br]
-## [code]is_global[/code] determines whether the sound can be played only in certain scene or global, the ones playing in the scene will be cut when calling [method Scenes.switch_to_scene][br]
-## [code]other_keys[/code] is a [Dictionary] to extend the playing method, please see the [color=purple]spell_keys[/color] above
+## Play a sound with given [AudioStream] resource and bind the sound player to a [Node2D][br]
+## [b]Note:[/b] This method creates [AudioStreamPlayer2D] which plays sound with pan changed according to its position to the center of screen, rather than [AudioStreamPlayer].[br]
+## [param resource] is the sound stream you are going to install[br]
+## [param ref] is the node to be bound[br]
+## [param is_global] determines whether the sound can be played only in certain scene or global, the ones playing in the scene will be cut when calling [method Scenes.switch_to_scene][br]
+## [param other_keys] is a [Dictionary] to extend the playing method, please see the [i][u]spell_keys[/u][/i] above
 func play_sound(resource: AudioStream, ref: Node2D, is_global: bool = true, other_keys: Dictionary = {}) -> void:
 	# Stop on empty sound to avoid crashes
 	if resource == null: return
@@ -76,12 +77,12 @@ func play_sound(resource: AudioStream, ref: Node2D, is_global: bool = true, othe
 	
 	if &"pitch" in other_keys && other_keys.pitch is float: player.pitch_scale = other_keys.pitch
 
-## Play a [color=green]sound[/color] with given [AudioStream] resource[br]
-## [color=orange][b]Note:[/b][/color] This method creates [AudioStreamPlayer], and please see [method play_sound] to learn their differences.[br]
-## [code]resource[/code] is the sound stream you are going to install[br]
-## [code]ref[/code] is the node to be bound[br]
-## [code]is_global[/code] determines whether the sound can be played only in certain scene or global, the ones playing in the scene will be cut when calling [method Scenes.switch_to_scene][br]
-## [code]other_keys[/code] is a [Dictionary] to extend the playing method, please see the [color=purple]spell_keys[/color] above
+## Play a sound with given [AudioStream] resource[br]
+## [b]Note:[/b] This method creates [AudioStreamPlayer], and please see [method play_sound] to learn their differences.[br]
+## [param resource] is the sound stream you are going to install[br]
+## [param ref] is the node to be bound[br]
+## [param is_global] determines whether the sound can be played only in certain scene or global, the ones playing in the scene will be cut when calling [method Scenes.switch_to_scene][br]
+## [param other_keys] is a [Dictionary] to extend the playing method, please see the [i][u]spell_keys[/u][i] above
 func play_1d_sound(resource: AudioStream, is_global: bool = true, other_keys: Dictionary = {}) -> void:
 	# Stop on empty sound to avoid crashes
 	if resource == null: return
@@ -92,11 +93,11 @@ func play_1d_sound(resource: AudioStream, is_global: bool = true, other_keys: Di
 	
 	if &"pitch" in other_keys && other_keys.pitch is float: player.pitch_scale = other_keys.pitch
 
-## Play a [color=cyan]music[/color] with given [AudioStream] resource and bind the sound player to a [Node2D].[br]
-## [color=orange][b]Note:[/b][/color] Musics played are [color=red][b]GLOBAL[/b][/color]![br]
-## [code]resource[/code] is the music stream you are going to install[br]
-## [code]channel_id[/code] is the channel to play the music[br]
-## [code]other_keys[/code] is a [Dictionary] to extend the playing method, please see the [color=purple]spell_keys[/color] above[br]
+## Play a [b]music[/b] with given [AudioStream] resource and bind the sound player to a [Node2D].[br]
+## [b]Note:[/b] Musics played are [b][u]GLOBAL[/u][/b]![br]
+## [param resource] is the music stream you are going to install[br]
+## [param channel_id] is the channel to play the music[br]
+## [param other_keys] is a [Dictionary] to extend the playing method, please see the [i][u]spell_keys[/u][/i] above[br]
 ## [color=red][b]WARNING:[/b][/color] if you have played one and then played another one in the same channel, the latter one will [b]OVERRIDE[/b] the former one and restart playing!
 ## [codeblock]
 ## # First you played a music in a channel
@@ -118,11 +119,11 @@ func play_music(resource: AudioStream, channel_id: int, other_keys: Dictionary =
 	if &"pitch" in other_keys && other_keys.pitch is float: _music_channels[channel_id].pitch_scale = other_keys.pitch
 
 ## Fade a music player that is playing, and you can choose it's way to fade.[br]
-## [code]player[/code] is the music player that is playing[br]
-## [code]to[/code] is the final [member AudioStreamPlayer.volume_db] you wish[br]
-## [code]weight[/code] is the strength/delta-value to fade the music[br]
-## [code]method[/code] is the way to fade the music, different [code]method[/code] decides different [code]weight[/code] calculation. See [enum FadingMethod][br]
-## [code]stop_after_fading[/code] determines whether the music stops playing after it fades to goal value. This is very useful when you are trying making fading-out-and-stop musics
+## [param player] is the music player that is playing[br]
+## [param to] is the final [member AudioStreamPlayer.volume_db] you wish[br]
+## [param weight] is the strength/delta-value to fade the music[br]
+## [param method] is the way to fade the music, different [param method] decides different [param weight] calculation. See [enum FadingMethod][br]
+## [param stop_after_fading] determines whether the music stops playing after it fades to goal value. This is very useful when you are trying making fading-out-and-stop musics
 func fade_music_1d_player(player: AudioStreamPlayer, to: float, weight: float, method: FadingMethod = FadingMethod.LINEAR, stop_after_fading: bool = false) -> void:
 	var has_player:bool
 	
