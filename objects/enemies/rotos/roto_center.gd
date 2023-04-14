70b9a0f8ec4afda1d3420b0f536fd49af9daac8c
reflexguru <trianglestudioad@gmail.com>
2023-04-14 23:18:37 +0300

    more arrange options for rotos

--- objects/enemies/rotos/roto_center.gd
+++ objects/enemies/rotos/roto_center.gd
@@ -10,14 +10,17 @@ extends Node2D
 
 enum ARRANGE_AS {
 	SELECT_ACTION,
-	BAR
+	BAR,
+	LINE_BAR,
+	CIRCLE
 }
 
 @export var arrange_as: ARRANGE_AS = ARRANGE_AS.SELECT_ACTION:
 	set(to):
 		match to:
-			ARRANGE_AS.BAR:
-				bar_like()
+			ARRANGE_AS.BAR: bar_like(false)
+			ARRANGE_AS.LINE_BAR: bar_like(true)
+			ARRANGE_AS.CIRCLE: circle_like()
 
 func switch_preview(on: bool) -> void:
 	for i in get_children():
@@ -26,7 +29,7 @@ func switch_preview(on: bool) -> void:
 		i.preview = on
 
 
-func bar_like() -> void:
+func bar_like(force_phase: bool) -> void:
 	var rotos: PackedVector2Array = []
 	for i in get_children():
 		if !i.is_in_group(&"#roto"):
@@ -40,3 +43,22 @@ func bar_like() -> void:
 		if !roto.is_in_group(&"#roto"):
 			continue
 		roto.amplitude = (rotos[0] / float(rotos.size())) * (j + 1)
+		if force_phase:
+			roto.phase = 0
+
+
+func circle_like() -> void:
+	var rotos: PackedVector2Array = []
+	for i in get_children():
+		if !i.is_in_group(&"#roto"):
+			continue
+		rotos.append(i.amplitude)
+	rotos.sort()
+	rotos.reverse()
+	
+	for j in get_child_count():
+		var roto: Node2D = get_child(j)
+		if !roto.is_in_group(&"#roto"):
+			continue
+		roto.amplitude = rotos[0]
+		roto.phase = (360.0 / float(rotos.size())) * j
