08bd23c3aec473830b82e7e0940d81764bc1d49b
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-04-17 13:58:23 +0800

    hammer bro attack

--- objects/enemies/koopa_bros/koopa_bro.gd
+++ objects/enemies/koopa_bros/koopa_bro.gd
@@ -13,6 +13,13 @@ extends GravityBody2D
 @export var attacking_count_unit: float = 0.06
 @export var attacking_chance: float = 0.09
 @export var attacking_delay: float = 0.6
+@export var projectile: InstanceNode2D
+@export var sound: AudioStream = preload("res://engine/objects/projectiles/sounds/throw.wav")
+@export_group("Animation")
+@export var sprite_projectile_animation_transform: Array[Transform2D] = [
+	Transform2D(0, Vector2.ZERO),
+	Transform2D(0, Vector2.ZERO)
+]
 
 var dir: int
 
@@ -31,6 +38,7 @@ var _jump_down: bool
 var _collision_mask: int
 
 @onready var sprite: AnimatedSprite2D = $Sprite
+@onready var sprite_projectile: Node2D = $SpriteProjectile
 @onready var timer_walk: Timer = $Walk
 @onready var timer_attack: Timer = $Attack
 
@@ -41,6 +49,7 @@ var _collision_mask: int
 
 func _physics_process(delta: float) -> void:
 	_direction()
+	_animation()
 	_bro_movement(delta)
 	motion_process(delta)
 
@@ -50,6 +59,18 @@ func _direction() -> void:
 	if !player: return
 	dir = Thunder.Math.look_at(global_position, player.global_position, global_transform)
 	sprite.flip_h = (dir < 0 && dir != 0)
+	sprite_projectile.flip_h = sprite.flip_h
+	pos_attack.position.x = pos_attack_x * dir
+
+
+func _animation() -> void:
+	if _step_attacking > 0:
+		sprite.play(&"attack")
+		sprite_projectile.visible = true
+		sprite_projectile.transform = pos_attack.transform * sprite_projectile_animation_transform[sprite.frame]
+	else:
+		sprite.play(&"default")
+		sprite_projectile.visible = false
 
 
 func _bro_movement(delta: float) -> void:
@@ -104,7 +125,22 @@ func _on_walk_timeout() -> void:
 
 
 func _on_attack_timeout() -> void:
-	pass
+	match _step_attacking:
+		0:
+			var chance: float = randf_range(0, 1)
+			if chance < attacking_chance:
+				_step_attacking = 1
+				timer_attack.start(attacking_delay)
+				timer_attack.one_shot = true
+		1:
+			_step_attacking = 0
+			timer_attack.start(attacking_count_unit)
+			timer_attack.one_shot = false
+			NodeCreator.prepare_ins_2d(projectile, self).call_method(
+				func(proj: Node2D) -> void:
+					proj.set(&"belongs_to", Data.PROJECTILE_BELONGS.ENEMY)
+			).execute_instance_script({bro = self}).create_2d().bind_global_transform(pos_attack.position)
+			Audio.play_sound(sound, self, false)
 
 
 func _on_jump() -> void:
---
+++ objects/enemies/koopa_bros/koopa_bro_hammer_throwing.gd
@@ -0,0 +1,8 @@
+extends ByNodeScript
+
+
+func _ready() -> void:
+	var min_speed: Vector2 = vars.get(&"speed_min", Vector2.ZERO)
+	var max_speed: Vector2 = vars.get(&"speed_max", Vector2.ZERO)
+	if node is GravityBody2D:
+		node.vel_set(Vector2(randf_range(min_speed.x, max_speed.x) * vars.bro.dir, randf_range(min_speed.y, max_speed.y)))
