f3a61f6f53070e877cab688152a6ff6a0dbc9c3a
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-04-28 16:47:09 +0800

    fixed missle bill and added animation for its launcher

--- objects/enemies/bullet_bill/bill/missile_bill.gd
+++ objects/enemies/bullet_bill/bill/missile_bill.gd
@@ -1,23 +1,14 @@
 extends GeneralMovementBody2D
 
 @export var speed_cap: float = 200
-@export var turn_speed: float = 5
+@export var turn_speed: float = 2.5
+
 var speed_modifier: float = 0
+
 
 func _physics_process(delta: float) -> void:
 	super(delta)
 	
-	var right_of_player: int = global_position.rotated(-global_rotation).x > Thunder._current_player.global_position.rotated(-global_rotation).x
-	if right_of_player == 0: right_of_player = -1
+	update_dir()
 	
-	speed.x = move_toward(speed.x, turn_speed * right_of_player, turn_speed * right_of_player)
-	speed.x = clamp(speed.x, -speed_cap, speed_cap)
-	
-	
-#	if dir == (1 if right_of_player else -1):
-#		if speed.x < speed_cap:
-#			accelerate_x(speed.x, turn_speed * dir)
-#		else:
-#			dir = -1 if right_of_player else 1
-#	elif abs(speed.x) > 0:
-#		speed.x -= turn_speed * Thunder.get_delta(delta)
+	speed.x = move_toward(speed.x, speed_cap * dir, abs(turn_speed * dir))
