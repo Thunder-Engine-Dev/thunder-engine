2a394e5e96640fcceec849cf5b3b622f96105ef7
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-05-08 23:02:21 +0800

    cringe roll-back

--- objects/powerups/instance_powerup.gd
+++ objects/powerups/instance_powerup.gd
@@ -1,10 +1,6 @@
 extends InstanceNode2D
 class_name InstancePowerup
 
-const NON_FALLBACKS: Array[PackedScene] = [
-	preload("res://engine/objects/items/coin/coin.tscn")
-]
-
 @export_category("InstanceNode2D")
 @export_group("Creation","creation_")
 @export var creation_fallback_node: PackedScene
@@ -17,7 +13,6 @@ func prepare() -> Variant:
 	if !creation_nodepack: return d_self
 	
 	if (
-		!d_self.creation_nodepack in NON_FALLBACKS &&
 		d_self.creation_fallback_node &&
 		d_self.creation_nodepack.resource_path != d_self.creation_fallback_node.resource_path &&
 		Thunder.is_player_power(Data.PLAYER_POWER.SMALL)
