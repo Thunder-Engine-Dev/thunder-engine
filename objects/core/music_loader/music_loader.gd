2d1d78df942d909b7681839b20f27dbbc75e9f19
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-06-28 18:47:20 +0800

    fixed music loader changing music

--- objects/core/music_loader/music_loader.gd
+++ objects/core/music_loader/music_loader.gd
@@ -15,6 +15,7 @@ func _ready() -> void:
 
 
 func _change_music(index: int, channel_id: int) -> void:
+#	if len(music) <= index: return
 	Audio.play_music(music[index], channel_id, { "ignore_pause": true })
 
 
