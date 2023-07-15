f6446eb18ae44afe5a6d62bf79660be2e756b44a
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-07-15 19:08:47 +0800

    added previous_scene_name to provide the environment for comparison

--- singletones/scripts/Scenes.gd
+++ singletones/scripts/Scenes.gd
@@ -21,6 +21,8 @@ signal scene_ready
 var _current_scene_buffer: PackedScene
 ## Current scene
 var current_scene: Node
+## Name of previous scene
+var previous_scene_name: StringName
 
 
 # Moves the current scene to viewport
@@ -33,6 +35,7 @@ func _ready() -> void:
 ## Loads a node as current scene, call with call_deferred
 func load_scene_deferred(scene: Node) -> void:
 	if !scene: return
+	previous_scene_name = current_scene.name
 	current_scene.free()
 	current_scene = scene
 	GlobalViewport.vp.add_child(current_scene)
@@ -44,6 +47,7 @@ func load_scene_deferred(scene: Node) -> void:
 ## Use with call_deferred
 func load_scene_from_packed(pck: PackedScene) -> void:
 	if !pck: return
+	previous_scene_name = current_scene.name
 	current_scene.free()
 	var scene: Node = pck.instantiate()
 	current_scene = scene
