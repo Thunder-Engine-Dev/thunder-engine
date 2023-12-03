c9bb8d4fd81e0b6495c5f87b377c252b960e2216
reflexguru <trianglestudioad@gmail.com>
2023-12-04 02:16:20 +0300

    fix focused state

--- scenes/main_menu/scripts/audio_selection.tres.gd
+++ scenes/main_menu/scripts/audio_selection.tres.gd
@@ -6,7 +6,7 @@ var change_sound = preload("res://engine/components/hud/sounds/scoring.wav")
 
 func _physics_process(delta: float) -> void:
 	super(delta)
-	if !focused: return
+	if !focused || !get_parent().focused: return
 	
 	if Input.is_action_just_pressed("ui_right"):
 		SettingsManager.settings[type] = clamp(SettingsManager.settings[type] + 0.1, 0, 1)
