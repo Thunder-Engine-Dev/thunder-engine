8275a2b182f4cdc239bffb0f933718b853fd7555
Lazy-Rabbit-2001 <2733679597@qq.com>
2024-02-15 19:08:37 +0800

    fixed fancy killing animation for upside-down piranhas

--- objects/enemies/_dead/enemy_killed.gd
+++ objects/enemies/_dead/enemy_killed.gd
@@ -1,24 +1,44 @@
 extends GeneralMovementBody2D
 
-var rotating_dir: int
+@export_range(0, 1, 0.001, "or_greater", "hide_slider", "suffix:px/s") var default_speed_x_min: float = 50
+@export_range(0, 1, 0.001, "or_greater", "hide_slider", "suffix:px/s") var default_speed_x_max: float = 125
+
+@onready var _original_global_rot: float = global_rotation
+
+var _sprite_rot_init: bool
+var _rotating_dir: int
+
+
+func _ready() -> void:
+	# Sprite setting
+	(func():
+		if !sprite_node:
+			for i in get_children():
+				if i is AnimatedSprite2D or i is Sprite2D:
+					sprite_node = i
+					break
+		if !_rotating_dir:
+			_rotating_dir = [-1, 1].pick_random()
+		if is_zero_approx(speed.x):
+			speed.x = _rotating_dir * randf_range(default_speed_x_min, default_speed_x_max)
+	).call_deferred()
+
 
 func _physics_process(delta: float) -> void:
 	super(delta)
 	
-	if !sprite_node:
-		for i in get_children():
-			if i is AnimatedSprite2D or i is Sprite2D:
-				sprite_node = i
-				break
-	
 	if sprite_node:
-		if rotating_dir:
-			sprite_node.rotation_degrees += 12 * 50 * rotating_dir * delta
-		else:
-			sprite_node.scale.y *= -1
+		if _sprite_rot_init:
+			_sprite_rot_init = true
+			sprite_node.rotation += _original_global_rot - _get_gravity_angle()
+		if _rotating_dir:
+			sprite_node.rotation_degrees += 12 * 50 * _rotating_dir * delta
 	
 	if !Thunder.view.screen_dir(global_position, get_global_gravity_dir(), 512):
 		queue_free()
 	
-	gravity_dir_rotation = false
-	gravity_dir = PhysicsServer2D.body_get_direct_state(get_rid()).total_gravity.normalized()
+	global_rotation = _get_gravity_angle()
+
+
+func _get_gravity_angle() -> float:
+	return PhysicsServer2D.body_get_direct_state(get_rid()).total_gravity.angle() - PI/2
