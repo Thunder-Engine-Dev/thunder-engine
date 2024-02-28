7efcf4c35c4ea8b3d681d442f68be96a2f1857e0
Lazy-Rabbit-2001 <2733679597@qq.com>
2024-02-28 12:19:03 +0800

    bubble effect + some fixes

--- objects/enemies/firebar/firebar.gd
+++ objects/enemies/firebar/firebar.gd
@@ -2,6 +2,7 @@
 extends Node2D
 
 const Fireball := preload("./fireball_firebar.tscn")
+const ClassFireball := preload("./fireball_firebar.gd")
 
 @export_category("Firebar")
 @export var preview: bool:
@@ -12,54 +13,63 @@ const Fireball := preload("./fireball_firebar.tscn")
 			return
 		
 		for i in get_children():
-			if i.is_in_group(&"firebar" + str(get_instance_id())):
+			if i is ClassFireball:
 				i.preview = preview
 				if !preview:
 					i.angle = angle
-@export_range(0, 100) var fireballs: int = 5:
+@export_range(1, 100) var fireballs: int = 5:
 	set(value):
 		fireballs = value
 		if is_node_ready():
-			_fireballs()
+			for i in get_children():
+				if i is ClassFireball:
+					i.queue_free()
+			for j in fireballs:
+				var f := Fireball.instantiate()
+				f.radius = 2 * ball_radius * j
+				f.angular_speed = angular_speed
+				f.angle = angle
+				f.position = Vector2.ZERO
+				add_child.call_deferred(f)
 @export_range(0, 256, 0.001, "suffix:px") var ball_radius: float = 8:
 	set(value):
 		ball_radius = value
 		if is_node_ready():
-			_fireballs()
+			var f := get_children()
+			for i in f.size():
+				if f[i] is ClassFireball:
+					f[i].radius = 2 * ball_radius * i
 @export_group("Physics")
 @export_range(-18000, 18000, 0.001, "suffix:°/s") var angular_speed: float = 100:
 	set(value):
 		angular_speed = value
 		if is_node_ready():
 			for i in get_children():
-				if i.is_in_group(&"firebar" + str(get_instance_id())):
+				if i is ClassFireball:
 					i.angular_speed = angular_speed
 @export_range(-180, 180, 0.001, "degrees") var angle: float:
 	set(value):
 		angle = value
 		if is_node_ready():
 			for i in get_children():
-				if i.is_in_group(&"firebar" + str(get_instance_id())):
+				if i is ClassFireball:
 					i.angle = angle
 
 
 func _ready() -> void:
 	# Create fireballs
-	_fireballs()
-	
+	fireballs = fireballs
 	set_deferred(&"preview", preview) # Triggers setter to iterate children for preview setting in deferred manner
 
-
-func _fireballs() -> void:
-	# Clear all
-	for i in get_children():
-		if i.is_in_group(&"firebar" + str(get_instance_id())):
-			i.queue_free()
-	# Create
-	for j in fireballs:
-		var f := Fireball.instantiate()
-		f.radius = 2 * ball_radius * j
-		f.angular_speed = angular_speed
-		f.angle = angle
-		f.add_to_group(&"firebar" + str(get_instance_id()))
-		add_child.call_deferred(f)
+#func _get_property_list() -> Array[Dictionary]:
+	#var properties: Array[Dictionary] = []
+	#
+	#properties.append({
+		#name = &"_init",
+		#type = TYPE_BOOL,
+		#usage = PROPERTY_USAGE_NO_EDITOR,
+		#hint = PROPERTY_HINT_NONE,
+		#hint_string = ""
+	#})
+	#
+	#return properties
