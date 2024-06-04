cf0edbf71175ed9963ae2df0c9eb1804e81dc880
jue131 <83157668+jue131@users.noreply.github.com>
2024-06-04 12:57:19 +0900

    fix podoboo launcher

--- objects/enemies/podoboo/podoboo_volcano_launcher.gd
+++ objects/enemies/podoboo/podoboo_volcano_launcher.gd
@@ -4,42 +4,42 @@ const PODOBOO = preload("res://engine/objects/enemies/podoboo/podoboo.tscn")
 
 @export var SHOOT_SOUND: AudioStream = preload("res://engine/objects/projectiles/sounds/shoot.wav")
 
+@export var launch_initial_delay: float = 0.5
 @export var launch_delay: float = 6
 @export var launch_interval: float = 0.3
-@export var launch_amount = 8
+@export var launch_amount: int = 8
 
 @export var podoboo_jumping_height: int = 256
 @export var podoboo_min_speed: int = -250
 @export var podoboo_max_speed: int = 250
 
-@onready var timer = $Timer
-@onready var shoot_timer = $ShootTimer
+@onready var timer: Timer = $Timer
+@onready var shoot_timer: Timer = $ShootTimer
 @onready var podoboo_icon = $Podoboo
 
 var _remaining_amount: int
 
 
 func _ready() -> void:
-	_interval()
-	
+	_interval(true)
 	podoboo_icon.queue_free()
 
 
-func _interval() -> void:
-	timer.start(launch_delay)
-	timer.connect("timeout", _shoot_start, CONNECT_ONE_SHOT)
+func _interval(init: bool = false) -> void:
+	timer.start(launch_initial_delay if init else launch_delay)
+	Thunder._connect(timer.timeout, _shoot_start, CONNECT_ONE_SHOT)
 
 
 func _shoot_start() -> void:
 	_remaining_amount = launch_amount
 	shoot_timer.start(launch_interval)
-	shoot_timer.connect("timeout", _shoot)
+	Thunder._connect(shoot_timer.timeout, _shoot)
 
 
 func _shoot() -> void:
 	if _remaining_amount <= 0:
 		shoot_timer.stop()
-		_interval()
+		_interval(false)
 		return
 	
 	Audio.play_sound(SHOOT_SOUND, self)
--- singletones/scripts/Thunder.gd
+++ singletones/scripts/Thunder.gd
@@ -52,6 +52,22 @@ func get_child_by_class_name(ref: Node, classname: String) -> Node:
 		if child.is_class(classname): return child
 	return null
 
+## Connects a signal to a callable without throwing errors if it's already connected
+@warning_ignore("int_as_enum_without_match", "int_as_enum_without_cast")
+func _connect(sig: Signal, callable: Callable, flags: ConnectFlags = 0) -> bool:
+	if callable.is_null() || !callable.is_valid(): return true
+	if sig.is_connected(callable): return true
+	sig.connect(callable, flags)
+	return false
+
+
+## Disconnects a signal from a callable without throwing errors if it's already disconnected
+func _disconnect(sig: Signal, callable: Callable) -> bool:
+	if callable.is_null() || !callable.is_valid(): return true
+	if !sig.is_connected(callable): return true
+	sig.disconnect(callable)
+	return false
+
 
 ## Gets relative FPS by inputting delta in [method Node._process] or [method Node._physics_process]
 func get_delta(delta: float) -> float:
