13cf4eccf0fa15f91d8566942dcb54df898dabc5
reflexguru <trianglestudioad@gmail.com>
2024-06-06 06:17:27 +0300

    bring back the 1 pixel fix by jue

--- objects/platform/platform_path.gd
+++ objects/platform/platform_path.gd
@@ -118,6 +118,9 @@ func _on_path_movement_process(delta: float) -> void:
 	# Emit Falling
 	if on_path && players_have_stood && falling_acceleration > 0.0 && falling_enabled:
 		on_path = false
+		# Fix the sprite "flickering" in the opposite direction for a couple of frames
+		if sprite_node:
+			sprite_node.position += falling_direction
 
 
 func _non_path_movement_process(delta: float) -> void:
