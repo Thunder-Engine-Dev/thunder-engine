79021e3b53bd6c3891a39ff06d2d2611483ed251
jue131 <83157668+jue131@users.noreply.github.com>
2024-06-20 05:41:33 +0900

    fix

--- objects/enemies/flameball_launcher/flameball_launcher_obstacle.gd
+++ objects/enemies/flameball_launcher/flameball_launcher_obstacle.gd
@@ -1,9 +1,15 @@
 extends AnimatableBody2D
 
-
-@onready var par: HBoxContainer = get_parent() as HBoxContainer
+@onready var base: Control = get_parent()
+@onready var shape: CollisionShape2D = $CollisionShape2D
 
 
 func _ready() -> void:
-	position = par.get_rect().get_center()
-	scale = par.get_rect().size
+	update()
+
+
+func update() -> void:
+	var length = base.get_rect().size
+	shape.scale = length
+	shape.position.y = sign(base.get_transform().get_scale().y) * length.y / 2
+	shape.position.x = sign(base.get_transform().get_scale().x) * length.x / 2
---
+++ objects/enemies/spikeball_launcher/spikeball.gd
@@ -0,0 +1,21 @@
+extends Projectile
+
+func _ready() -> void:
+	if speed.x < 0 && sprite_node:
+		sprite_node.rotation_speed = -sprite_node.rotation_speed
+
+
+func _on_trail() -> void:
+	if !sprite_node:
+		return
+	Effect.trail(self, sprite_node.texture, Vector2.ZERO, sprite_node.flip_h).rotation = sprite_node.rotation
+
+
+func _on_level_end() -> void:
+	if !Thunder.view.is_getting_closer(self, 32):
+		if Thunder.view.is_getting_closer(self, 320):
+			queue_free()
+		return
+	Data.values.score += 200
+	ScoreText.new(str(200), self)
+	queue_free()
