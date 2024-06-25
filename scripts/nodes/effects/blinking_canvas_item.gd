5272b2e4c93af04dfa85a4669d632d4a6ce86fce
reflexguru <trianglestudioad@gmail.com>
2024-06-25 03:08:13 +0300

    add _min_a to blinking canvas item

--- scripts/nodes/effects/blinking_canvas_item.gd
+++ scripts/nodes/effects/blinking_canvas_item.gd
@@ -1,7 +1,7 @@
 extends CanvasItem
 
 var _tw: Tween
-
+var _min_a: float = 0
 
 func _ready() -> void:
 	modulate.a = 0
@@ -12,4 +12,4 @@ func _physics_process(delta: float) -> void:
 	
 	_tw = create_tween().set_loops().set_trans(Tween.TRANS_SINE)
 	_tw.tween_property(self, ^"modulate:a", 1, 0.5)
-	_tw.tween_property(self, ^"modulate:a", 0, 0.5)
+	_tw.tween_property(self, ^"modulate:a", _min_a, 0.5)
