c538fbe206f5585c1aaf8edfac7415999e714109
reflexguru <trianglestudioad@gmail.com>
2024-07-25 12:57:02 +0300

    fix enemies getting stuck together in certain situations

--- objects/enemies/enemy_body_collision.gd
+++ objects/enemies/enemy_body_collision.gd
@@ -4,11 +4,25 @@ extends Area2D
 @export var solid: bool = true
 @export var turn_back: bool = true
 
+var _prev_collided_areas = {}
+
 
 func _ready() -> void:
 	area_entered.connect(
 		func(area: Area2D) -> void:
 			if area.get_script() != get_script() || area.owner == owner: return
-			if owner is GravityBody2D && area.solid && turn_back:
+			if owner is GravityBody2D && area.solid && turn_back && !(area.get_instance_id() in _prev_collided_areas):
 				owner.turn_x()
+			
+			_prev_collided_areas[area.get_instance_id()] = 0
 	)
+
+
+func _physics_process(delta: float) -> void:
+	var collided_areas = get_overlapping_areas()
+	for area in _prev_collided_areas:
+		var instance = instance_from_id(area)
+		if !is_instance_valid(instance) || !(instance in collided_areas):
+			_prev_collided_areas[area] += 10 * delta
+			if _prev_collided_areas[area] > 4:
+				_prev_collided_areas.erase(area)
