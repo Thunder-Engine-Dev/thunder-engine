1b5bee3aef225fe6ff578d56c8bf3afe69dadf29
jue131 <83157668+jue131@users.noreply.github.com>
2024-08-04 21:41:54 +0900

    spike ceiling fix

--- objects/enemies/spike_ceiling/spike_ceiling.gd
+++ objects/enemies/spike_ceiling/spike_ceiling.gd
@@ -11,7 +11,6 @@ const FALL = preload("res://engine/objects/enemies/spike_ceiling/sfx/fall.wav")
 @export var reabilitation_delay: float = 2.0
 @export var reabilitation_speed: float = 100.0
 
-var _activated: bool = false
 var _state: int = 0
 var _sine: float
 var _falling_vel: float
@@ -31,12 +30,12 @@ func _physics_process(delta: float) -> void:
 	if player && activated_area && activated_area.has_point(player.global_position):
 		if timer.is_stopped():
 			timer.start(activation_time)
-		_activated = true
 	elif !timer.is_stopped():
+		_sine = 0
 		timer.stop()
 	
 	var pos_size: float = global_position.y + size.y
-	if _state == 1:
+	if _state == 1 && !timer.is_stopped():
 		_sine += 50 * delta
 		global_position.y = init_pos.y + sin(_sine) * (_sine / 70.0)
 	elif _state == 2:
@@ -47,7 +46,7 @@ func _physics_process(delta: float) -> void:
 			_falling_vel = 0
 			Thunder._current_camera.shock_smooth(10, 5)
 			Audio.play_1d_sound(FALL)
-			await get_tree().create_timer(3.0).timeout
+			await get_tree().create_timer(2.0, false).timeout
 			_state = 4
 	elif _state == 4:
 		global_position.y = move_toward(global_position.y, init_pos.y, reabilitation_speed * delta)
@@ -69,5 +68,6 @@ func _on_activation_timeout() -> void:
 
 
 func _set_scale() -> void:
-	area.scale = Vector2(get_rect().size.x, spike.get_rect().size.y)
-	area.position = spike.get_rect().size / 2
+	var _size := Vector2(get_rect().size.x, spike.get_rect().size.y)
+	area.scale = _size
+	area.position = _size / 2
