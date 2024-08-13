5714f4d0ed1cc996f4dcd2c0503a49feffe29e21
jue131 <83157668+jue131@users.noreply.github.com>
2024-08-13 23:03:44 +0900

    slight refactor of settingsmanager.gd

--- singletones/scripts/SettingsManager.gd
+++ singletones/scripts/SettingsManager.gd
@@ -8,11 +8,12 @@ enum QUALITY {
 
 const settings_path = "user://settings.thss"
 const tweaks_path = "user://tweaks.thss"
+var custom_file_paths: Dictionary = {}
 
-var default_settings = {
+var default_settings: Dictionary = {
 	"sound": 1,
 	"music": 1,
-	"quality": QUALITY.MAX,
+	"quality": ProjectSettings.get_setting("application/thunder_settings/default_quality_setting", QUALITY.MAX),
 	"game_speed": 1,
 	"autopause": true,
 	"vsync": true,
@@ -20,7 +21,6 @@ var default_settings = {
 	"physics_tps": 0,
 	"filter": false,
 	"fullscreen": false,
-	"first_launch": true,
 	"controls": {
 		"m_up": _get_current_key(&"m_up"),
 		"m_down": _get_current_key(&"m_down"),
@@ -31,9 +31,11 @@ var default_settings = {
 		"m_attack": _get_current_key(&"m_attack"),
 		"pause_toggle": _get_current_key(&"pause_toggle"),
 	},
+	"character": ProjectSettings.get_setting("application/thunder_settings/default_character_setting", "Mario"),
+	"custom": {},
 }
 
-var settings = default_settings.duplicate(true)
+var settings: Dictionary = default_settings.duplicate(true)
 var no_saved_settings: bool = false
 
 var tweaks: Dictionary = {}
@@ -44,76 +46,34 @@ signal settings_loaded
 signal tweaks_updated
 signal tweaks_saved
 signal tweaks_loaded
+signal data_saved(id: String)
+signal data_loaded(id: String)
 
 func _ready() -> void:
 	load_settings()
 	load_tweaks()
 
-## Get ProjectSettings "tweak" located in path "application/thunder_settings/tweaks"
+## Returns a ProjectSettings "tweak" located in path "application/thunder_settings/tweaks"
 func get_tweak(tweak_name: String, default_value: Variant = null) -> Variant:
 	return ProjectSettings.get_setting("application/thunder_settings/tweaks/" + tweak_name, default_value)
 
-## Set ProjectSettings "tweak" to a new value
+## Sets a ProjectSettings "tweak" to a new value
 func set_tweak(tweak_name: String, value: Variant) -> void:
 	ProjectSettings.set_setting("application/thunder_settings/tweaks/" + tweak_name, value)
 	tweaks[tweak_name] = value
 	tweaks_updated.emit()
 
-## Returns the key label of specified action
-func _get_current_key(action: StringName) :
-	var keys = InputMap.action_get_events(action)
-	for key in keys:
-		if key is InputEventKey:
-			return key.as_text().get_slice(' (', 0)
+## Returns a project-specific custom setting from settings variable
+func get_custom_setting(setting: String, default_value: Variant = null) -> Variant:
+	if setting in settings.custom:
+		return settings.custom[setting]
+	settings.custom[setting] = default_value
+	return default_value
 
-## Loads controls settings to InputMap
-func _load_keys() -> void:
-	var controls = settings.controls
-	for action in controls:
-		if controls[action] and controls[action] is String:
-			var scancode = OS.find_keycode_from_string(controls[action])
-			var key = InputEventKey.new()
-			key.keycode = scancode
-			if key is InputEventKey:
-				var oldKeys = InputMap.action_get_events(action)
-				for toRemove in oldKeys:
-					if toRemove is InputEventKey:
-						InputMap.action_erase_event(action, toRemove)
-				InputMap.action_add_event(action, key)
-	
-	print("[Settings Manager] Loaded input maps from settings.")
+## Sets a project-specific custom setting to a new value
+func set_custom_setting(setting: String, value: Variant = null) -> void:
+	settings.custom[setting] = value
 
-## Saves the settings variable to file
-func save_settings() -> void:
-	var data = JSON.stringify(settings)
-	
-	var file: FileAccess = FileAccess.open(settings_path, FileAccess.WRITE)
-	file.store_string(data)
-	file.close()
-	
-	settings_saved.emit()
-	print("[Settings Manager] Settings saved!")
-
-## Loads the settings variable from file
-func load_settings() -> void:
-	var path: String = settings_path
-	if !FileAccess.file_exists(path):
-		print("[Settings Manager] Using the default settings, no saved ones.")
-		no_saved_settings = true
-		return
-	
-	var data: String = FileAccess.get_file_as_string(path)
-	var dict = JSON.parse_string(data)
-	
-	if dict == null:
-		OS.alert("Failed to load saved settings " + name, "Can't load save file!")
-		return
-	
-	settings = dict
-	_check_for_validity()
-	_process_settings()
-	settings_loaded.emit()
-	print("[Settings Manager] Loaded settings from a file.")
 
 func _check_for_validity() -> void:
 	for i in default_settings.keys():
@@ -137,6 +97,7 @@ func _process_settings() -> void:
 	
 	# Fullscreen
 	if !settings.fullscreen && DisplayServer.window_get_mode(0) == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+		# This is needed to avoid borders being outside the monitor boundaries when you exit fullscreen
 		if OS.get_name() == "Windows":
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MINIMIZED)
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
@@ -148,6 +109,7 @@ func _process_settings() -> void:
 	if settings.physics_tps <= 0:
 		var rate: int = ceili(DisplayServer.screen_get_refresh_rate())
 		if rate < 119:
+			# Physics rate is always set to be at least around 120 TPS for better collisions
 			Engine.physics_ticks_per_second = rate * 2
 			print(&"Using double tps for physics")
 		else:
@@ -177,8 +139,33 @@ func _process_settings() -> void:
 	
 	settings_updated.emit()
 
+## Returns the key label of specified action
+func _get_current_key(action: StringName) :
+	var keys = InputMap.action_get_events(action)
+	for key in keys:
+		if key is InputEventKey:
+			return key.as_text().get_slice(' (', 0)
+
+## Loads controls settings to InputMap
+func _load_keys() -> void:
+	var controls = settings.controls
+	for action in controls:
+		if controls[action] and controls[action] is String:
+			var scancode = OS.find_keycode_from_string(controls[action])
+			var key = InputEventKey.new()
+			key.keycode = scancode
+			if key is InputEventKey:
+				var oldKeys = InputMap.action_get_events(action)
+				for toRemove in oldKeys:
+					if toRemove is InputEventKey:
+						InputMap.action_erase_event(action, toRemove)
+				InputMap.action_add_event(action, key)
+	
+	print("[Settings Manager] Loaded input maps from settings.")
+
 
 var old_scale: float = -1
+## Handles scaling the window via the Scale menu option
 func _window_scale_logic(force_update: bool = false) -> void:
 	if no_saved_settings: return
 	if settings.scale == 0: return
@@ -201,34 +188,71 @@ func _window_scale_logic(force_update: bool = false) -> void:
 	
 	old_scale = settings.scale
 
+
+## Saves the settings variable to file
+func save_settings() -> void:
+	save_data(settings, settings_path, "Settings")
+	
+	settings_saved.emit()
+	print("[Settings Manager] Settings saved!")
+
+## Loads the settings variable from file
+func load_settings() -> void:
+	var loaded_data: Dictionary = load_data(settings_path, "Settings")
+	if loaded_data.is_empty():
+		no_saved_settings = true
+		return
+	
+	settings = loaded_data
+	_check_for_validity()
+	_process_settings()
+	settings_loaded.emit()
+	print("[Settings Manager] Loaded settings from a file.")
+
+
 ## Saves the tweaks variable to a file
 func save_tweaks() -> void:
-	var data = JSON.stringify(tweaks)
-	
-	var file: FileAccess = FileAccess.open(tweaks_path, FileAccess.WRITE)
-	file.store_string(data)
-	file.close()
+	save_data(tweaks, tweaks_path, "Tweaks")
 	
 	tweaks_saved.emit()
 	print("[Settings Manager] Tweaks saved!")
 
-## Loads the settings variable from file
+## Loads the tweaks variable from file
 func load_tweaks() -> void:
-	if !FileAccess.file_exists(tweaks_path):
-		print("[Settings Manager] Using the default tweaks, no saved ones.")
+	var loaded_data: Dictionary = load_data(tweaks_path, "Tweaks")
+	if loaded_data.is_empty():
 		return
 	
-	var data: String = FileAccess.get_file_as_string(tweaks_path)
-	var dict = JSON.parse_string(data)
-	
-	if dict == null:
-		OS.alert("Failed to load saved tweaks " + name, "Can't load save file!")
-		return
-	
-	tweaks = dict
+	tweaks = loaded_data
 	for tweak in tweaks:
 		var value = tweaks[tweak]
 		ProjectSettings.set_setting("application/thunder_settings/tweaks/" + tweak, value)
-	
 	tweaks_loaded.emit()
 	print("[Settings Manager] Loaded tweaks from a file.")
+
+
+## Loads the data
+func load_data(from_path: String, id: String = "Custom") -> Dictionary:
+	if !FileAccess.file_exists(from_path):
+		print("[Settings Manager] %s: File does not exist. Using default values." % id)
+		return {}
+	
+	var json: String = FileAccess.get_file_as_string(from_path)
+	var dict = JSON.parse_string(json)
+	
+	if dict == null:
+		OS.alert("Failed to load saved %s data %s" % [id, name], "Can't load save file!")
+		return {}
+	
+	data_loaded.emit(id)
+	return dict
+
+## Saves the data
+func save_data(data: Dictionary, to_path: String, id: String = "Custom") -> void:
+	var json: String = JSON.stringify(data)
+	
+	var file: FileAccess = FileAccess.open(to_path, FileAccess.WRITE)
+	file.store_string(json)
+	file.close()
+	
+	data_saved.emit(id)
