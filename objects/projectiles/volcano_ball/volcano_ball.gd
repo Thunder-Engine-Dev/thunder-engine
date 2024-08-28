bb0a13be3d0fc3e9ab5a7af2f65ce5a528e556fd
reflexguru <trianglestudioad@gmail.com>
2024-08-28 11:20:13 +0300

    implement volcano

---
+++ objects/projectiles/volcano_ball/volcano_ball.gd
@@ -0,0 +1,19 @@
+extends Projectile
+
+func _ready() -> void:
+	if speed.x < 0 && sprite_node:
+		sprite_node.rotation_speed = -sprite_node.rotation_speed
+	
+	if sprite_node:
+		var tw = create_tween()
+		tw.tween_property(sprite_node, "scale", Vector2.ONE, 0.05)
+
+
+func _on_level_end() -> void:
+	if !Thunder.view.is_getting_closer(self, 32):
+		if Thunder.view.is_getting_closer(self, 320):
+			queue_free()
+		return
+	Data.values.score += 200
+	ScoreText.new(str(200), self)
+	queue_free()
