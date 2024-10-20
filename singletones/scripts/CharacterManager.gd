cc5de43e3ea23dd28bdaa6d0910b35a9792418c2
jue131 <83157668+jue131@users.noreply.github.com>
2024-10-20 11:20:42 +0900

    upd: power console command

--- singletones/scripts/CharacterManager.gd
+++ singletones/scripts/CharacterManager.gd
@@ -32,11 +32,44 @@ func _ready() -> void:
 	print(suits)
 
 
-func get_suit(suit_name: String) -> PlayerSuit:
+func get_suit(suit_name: String, character_name: String = "") -> PlayerSuit:
 	var player = Thunder._current_player
-	if player && player.character in suits && suit_name in suits[player.character]:
-		return suits[player.character][suit_name]
+	var chara: String = character_name
+	if chara.is_empty() && player && player.character in suits:
+		chara = player.character
+	
+	if chara && chara in suits && suit_name in suits[chara]:
+		return suits[chara][suit_name]
 	return null
+
+
+func get_suit_names(character_name: String = "") -> Array:
+	var player = Thunder._current_player
+	var chara: String = character_name
+	if chara.is_empty() && player && player.character in suits:
+		chara = player.character
+	
+	var suit_name_arr: Array = []
+	if chara && chara in suits:
+		for key in suits[chara].keys():
+			suit_name_arr.append(key)
+	return suit_name_arr
+
+
+func get_character_names() -> Array:
+	var chara_name_arr: Array = []
+	for key in suits.keys():
+		chara_name_arr.append(key)
+	return chara_name_arr
+
+
+func add_suit(suit: PlayerSuit, power: String, character: String, override: bool = false) -> void:
+	var new_suit_dict: Dictionary = suits.duplicate(true) 
+	if !character in new_suit_dict:
+		new_suit_dict[character] = {}
+	if !override && power in new_suit_dict[character]:
+		return
+	new_suit_dict[character][power] = suit
 
 
 func add_suits(dict: Dictionary, character: String, override: bool = false) -> void:
