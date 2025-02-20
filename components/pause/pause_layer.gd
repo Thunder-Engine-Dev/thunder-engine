85dbfc186f975e636686c9ee58b073e97788e2b3
Lazy-Rabbit-2001 <2733679597@qq.com>
2025-02-20 22:58:39 +0800

    fixed the pause becomes unusable; made the onready properties statically typed in viewport.gd

--- components/pause/pause_layer.gd
+++ components/pause/pause_layer.gd
@@ -3,5 +3,5 @@ extends CanvasLayer
 
 func _ready() -> void:
 	get_parent().remove_child.call_deferred(self)
-	await GlobalViewport.ready
+	await get_tree().process_frame
 	GlobalViewport.vp.add_child.call_deferred(self)
