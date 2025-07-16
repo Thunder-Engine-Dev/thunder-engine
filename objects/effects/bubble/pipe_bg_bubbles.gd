3902dc0525836ea22037d5feb0b3a1aab93ccf45
jue131 <83157668+jue131@users.noreply.github.com>
2025-07-17 00:35:23 +0900

    fix: container bubbles; p_switch detection

--- objects/effects/bubble/pipe_bg_bubbles.gd
+++ objects/effects/bubble/pipe_bg_bubbles.gd
@@ -5,6 +5,7 @@ const BUBBLE_BG = preload("res://engine/objects/effects/bubble/bubble_bg.tscn")
 @export var gen_z_index: int = -30
 @export var min_speed: float = 40
 @export var max_speed: float = 250
+@export var delay_range_sec := Vector2(0.4, 2.0)
 
 var counter: float = 0
 var count_to: float = 0
@@ -14,7 +15,7 @@ func _physics_process(delta: float) -> void:
 		return
 	counter += delta
 	if counter > count_to:
-		count_to = randf_range(0.4, 2.0)
+		count_to = randf_range(delay_range_sec.x, delay_range_sec.y)
 		counter = 0
 		var buble = BUBBLE_BG.instantiate()
 		buble.speed = Vector2.UP.rotated(global_rotation) * randf_range(min_speed, max_speed)
