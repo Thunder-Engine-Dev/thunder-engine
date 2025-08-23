8cf17dab22864155eb566e037175fbce223563ff
jue131 <83157668+jue131@users.noreply.github.com>
2025-08-24 02:23:07 +0900

    feat(setplayervar): 'persist' argument

--- singletones/nodes/debug/console/commands/setplayervar.gd
+++ singletones/nodes/debug/console/commands/setplayervar.gd
@@ -1,10 +1,14 @@
 extends Command
 
+var persist_vars: Dictionary = {}
+
 static func register() -> Command:
 	return new().set_name("setplayervar") \
 	.add_param("property", TYPE_STRING, true) \
 	.add_param("value", TYPE_FLOAT, true) \
-	.set_description("Modifies Player Config. Without arguments, will dump a list of properties to modify.")
+	.add_param("persist?", TYPE_BOOL, true) \
+	.set_description("Modifies Player Config. Without arguments, will dump a list of properties to modify. If the third argument is true, the command will automatically run on level restart.")
+
 
 func execute(args:Array) -> Command.ExecuteResult:
 	var result: Command.ExecuteResult = Command.ExecuteResult.new("Error: Player not found.")
@@ -24,4 +28,24 @@ func execute(args:Array) -> Command.ExecuteResult:
 		return Command.ExecuteResult.new("Error: Property not found.")
 	if args[0] == "script": return Command.ExecuteResult.new(Error.Wrong)
 	pl.suit.physics_config.set(args[0], args[1])
+	if len(args) >= 3 && !args[2].is_empty():
+		if ["no", "false", "0", "off", "disable", "remove"].has(args[2].to_lower()):
+			persist_vars.erase(args[0])
+			if persist_vars.is_empty():
+				Thunder._disconnect(Scenes.scene_ready, patch_level)
+			return Command.ExecuteResult.new("Success, persistence from this property removed")
+		elif ["yes", "true", "1", "on", "enable", "add"]:
+			if persist_vars.is_empty():
+				Thunder._connect(Scenes.scene_ready, patch_level)
+			persist_vars[args[0]] = args[1]
+			return Command.ExecuteResult.new("Success, added persistence to this property")
 	return Command.ExecuteResult.new("Success")
+
+func patch_level() -> void:
+	if !Scenes.is_inside_tree():
+		return
+	var pl: Player = Thunder._current_player
+	if !pl:
+		return
+	for i in persist_vars.keys():
+		pl.suit.physics_config.set(i, persist_vars[i])
