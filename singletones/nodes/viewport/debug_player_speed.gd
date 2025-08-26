2b1fd1a8a8a95ebc5e775f1da2c194a37f07e197
jue131 <83157668+jue131@users.noreply.github.com>
2025-08-26 17:12:09 +0900

    feat: show player speed with `--speedrun-stats` launch argument
    note: using this launch argument is not considered cheating

---
+++ singletones/nodes/viewport/debug_player_speed.gd
@@ -0,0 +1,18 @@
+extends Label
+
+@onready var template := "%.2f\n%.2f"
+
+func _ready() -> void:
+	hide()
+	if !OS.get_cmdline_user_args().has("--speedrun-stats"):
+		queue_free()
+		return
+	show()
+
+func _physics_process(delta: float) -> void:
+	var pl := Thunder._current_player
+	if !pl:
+		text = "--"
+		return
+	text = template % [pl.speed.x, pl.speed.y]
+	
