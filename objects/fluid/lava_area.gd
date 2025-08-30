9a3eb6518abc51870748f083b3ef380d0275e8ab
jue131 <83157668+jue131@users.noreply.github.com>
2025-08-31 00:37:22 +0900

    style: lava_area connections in their own methods

--- objects/fluid/lava_area.gd
+++ objects/fluid/lava_area.gd
@@ -7,48 +7,50 @@ signal area_got_in_lava_at(pos: Vector2)
 
 func _ready() -> void:
 	# Body in/out of water
-	body_entered.connect(
-		func(body: Node2D) -> void:
-			if !is_instance_valid(body) || body.is_queued_for_deletion():
-				return
-			if body == Thunder._current_player:
-				if !body.is_starman():
-					body.die()
-				elif body.has_node("Underwater") && !body.get(&"is_underwater"):
-					var underwater: Node = body.get_node("Underwater")
-					underwater.in_water()
-					Thunder._connect(body.timer_starman.timeout, _player_death_after_starman, CONNECT_ONE_SHOT)
-			if body.is_in_group(&"#lava_body"):
-				self._spray.call_deferred(body, Vector2.ZERO)
-				if body.has_method(&"got_in_lava"):
-					body.got_in_lava()
-					body_got_in_lava_at.emit(body.global_position)
-	)
-	area_entered.connect(
-		func(area: Area2D) -> void:
-			if area.is_in_group(&"#lava_body"):
-				self._spray.call_deferred(area, Vector2.ZERO)
-				if area.has_method(&"got_in_lava"):
-					area.got_in_lava()
-					area_got_in_lava_at.emit(area.global_position)
-			if area.has_node(^"EnemyAttacked"):
-				var enemy_att = area.get_node(^"EnemyAttacked")
-				if enemy_att.killing_immune.has(&"lava"):
-					if enemy_att.killing_immune.lava == false:
-						enemy_att.got_killed(&"lava", [&"no_score"])
-	)
-	
-	body_exited.connect(
-		func(body: Node2D) -> void:
-			if !is_instance_valid(body) || body.is_queued_for_deletion():
-				return
-			if body == Thunder._current_player && body.is_starman() && body.has_node("Underwater") && body.get(&"is_underwater"):
-				var underwater: Node = body.get_node("Underwater")
-				underwater.out_of_water()
-				Thunder._disconnect(body.timer_starman.timeout, _player_death_after_starman)
-			if body.is_in_group(&"#lava_body"):
-				self._spray.call_deferred(body, Vector2.ZERO)
-	)
+	body_entered.connect(_on_body_entered)
+	area_entered.connect(_on_area_entered)
+	body_exited.connect(_on_body_exited)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group(&"#lava_body"):
+		self._spray.call_deferred(area, Vector2.ZERO)
+		if area.has_method(&"got_in_lava"):
+			area.got_in_lava()
+			area_got_in_lava_at.emit(area.global_position)
+	if area.has_node(^"EnemyAttacked"):
+		var enemy_att = area.get_node(^"EnemyAttacked")
+		if enemy_att.killing_immune.has(&"lava"):
+			if enemy_att.killing_immune.lava == false:
+				enemy_att.got_killed(&"lava", [&"no_score"])
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if !is_instance_valid(body) || body.is_queued_for_deletion():
+		return
+	if body == Thunder._current_player:
+		if !body.is_starman():
+			body.die()
+		elif body.has_node("Underwater") && !body.get(&"is_underwater"):
+			var underwater: Node = body.get_node("Underwater")
+			underwater.in_water()
+			Thunder._connect(body.timer_starman.timeout, _player_death_after_starman, CONNECT_ONE_SHOT)
+	if body.is_in_group(&"#lava_body"):
+		self._spray.call_deferred(body, Vector2.ZERO)
+		if body.has_method(&"got_in_lava"):
+			body.got_in_lava()
+			body_got_in_lava_at.emit(body.global_position)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if !is_instance_valid(body) || body.is_queued_for_deletion():
+		return
+	if body == Thunder._current_player && body.is_starman() && body.has_node("Underwater") && body.get(&"is_underwater"):
+		var underwater: Node = body.get_node("Underwater")
+		underwater.out_of_water()
+		Thunder._disconnect(body.timer_starman.timeout, _player_death_after_starman)
+	if body.is_in_group(&"#lava_body"):
+		self._spray.call_deferred(body, Vector2.ZERO)
 
 
 func _spray(on: Node2D, offset: Vector2) -> void:
@@ -57,6 +59,7 @@ func _spray(on: Node2D, offset: Vector2) -> void:
 			if on is GravityBody2D:
 				spray.translate(Vector2.UP * on.speed.y * on.get_physics_process_delta_time())
 	)
+
 
 func _player_death_after_starman() -> void:
 	var pl := Thunder._current_player
