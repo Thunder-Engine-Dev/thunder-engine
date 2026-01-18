8951b5880740f31ebe7be4a120f1e0a8408cd08f
YuuKaana <2733679597@qq.com>
2026-01-18 17:27:27 +0800

    fixed mario getting stuck at the ceiling-wall corners

--- scripts/classes/corrected_character_body_2d/corrected_character_body_2d.gd
+++ scripts/classes/corrected_character_body_2d/corrected_character_body_2d.gd
@@ -59,6 +59,7 @@ func horizontal_correction(amount: int) -> void:
 # Tile gap runover
 func vertical_correction(amount: int) -> void:
 	if is_on_floor(): return
+	if is_on_wall(): return # Fixes bodys get stuck at the corner if the bodys is moving up along the wall and reaches the ceiling.
 	if velocity.y <= 0 or abs(velocity.x) <= 1: return
 	
 	var delta = get_physics_process_delta_time()
