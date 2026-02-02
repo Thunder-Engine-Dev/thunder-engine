6a382564ed8740628da269f32e1652175fb09df8
reflexguru <trianglestudioad@gmail.com>
2026-02-02 23:29:55 +0500

    feat(centipede): loop path support, trigger types, immediate trigger type, mute sound on path end

--- objects/centipede/centipede.gd
+++ objects/centipede/centipede.gd
@@ -2,19 +2,27 @@ extends StaticBody2D
 
 const CentipedePathpoint := preload("centipede_pathpoint.gd")
 
+enum CentipedeTriggerType {
+	TRIGGER_AREA,
+	IMMEDIATE,
+	NONE
+}
+
 @export_category("Centipede")
 @export var moving_sound := preload("sfx/idle.wav")
-@export var block_amount: int = 5
+@export var stop_sound_on_path_end: bool = true
 @export var speed: float = 100
 @export_range(0.01, 20, 0.01, "suffix:s") var waiting: float = 0.25
 @export_group("Path")
 @export_node_path("Node2D") var centipede_pathpoints_path: NodePath = ^"CentipedePathpoints"
+@export var loop_path: bool = false
 @export_group("Convey-belt Like")
 @export var convey_belt_speed: Vector2
 @export_group("Trigger", "trigger_")
-@export var trigger_area_enabled: bool = true:
+@export var trigger_type: CentipedeTriggerType = CentipedeTriggerType.TRIGGER_AREA
+@export var trigger_area_draw_enabled: bool = true:
 	set(to):
-		trigger_area_enabled = to
+		trigger_area_draw_enabled = to
 		if Engine.is_editor_hint() && has_node("DrawArea"):
 			$DrawArea.queue_redraw()
 @export var trigger_area: Rect2 = Rect2(0, 0, 32, 480):
@@ -34,8 +42,10 @@ var is_moving: bool
 var _movement_blocked: bool = true
 var _beginning: Vector2
 var _has_moved: bool = false
+var _direction: int = 1
 
 var step_next_points: PackedVector2Array
+var initial_step_next_points: PackedVector2Array
 
 @onready var running_speed: float = speed:
 	set(value):
@@ -47,12 +57,15 @@ func _ready() -> void:
 	
 	_sign_up_step_points()
 	global_position = _beginning
+	
+	if trigger_type == CentipedeTriggerType.IMMEDIATE:
+		is_moving = true
 
 func _process(delta: float) -> void:
 	_rotate_spikeball(delta)
 
 func _physics_process(delta: float) -> void:
-	if is_instance_valid(player) && trigger_area_enabled:
+	if is_instance_valid(player) && trigger_type == CentipedeTriggerType.TRIGGER_AREA:
 		_detect_area()
 	
 	if !is_moving:
@@ -65,15 +78,26 @@ func _physics_process(delta: float) -> void:
 			audio_player.stream = moving_sound
 			audio_player.play()
 	
-	if !_movement_blocked && !step_next_points.is_empty():
-		var next := step_next_points[0]
-		global_position = global_position.move_toward(next, speed * delta)
-		if global_position.is_equal_approx(next):
-			global_position = next
-			step_next_points.remove_at(0)
-			_movement_blocked = true
-			timer.start(waiting)
-
+	if !_movement_blocked:
+		if loop_path && step_next_points.is_empty():
+			_reset_step_points()
+			global_position = _beginning
+		
+		if !loop_path && step_next_points.is_empty() && audio_player.playing && stop_sound_on_path_end:
+			audio_player.stop()
+		
+		if !step_next_points.is_empty():
+			var next := step_next_points[0]
+			var old_pos = global_position
+			global_position = global_position.move_toward(next, speed * delta)
+			if global_position.is_equal_approx(next):
+				global_position = next
+				step_next_points.remove_at(0)
+				_movement_blocked = true
+				timer.start(waiting)
+			else:
+				_calculate_direction(old_pos, global_position)
+	
 
 func _sign_up_step_points() -> void:
 	for i in centipede_pathpoints.get_children():
@@ -82,6 +106,14 @@ func _sign_up_step_points() -> void:
 		step_next_points.append(i.global_position)
 	if speed < 0.0:
 		step_next_points.reverse()
+	_beginning = step_next_points[0]
+	initial_step_next_points = step_next_points.duplicate()
+	step_next_points.remove_at(0)
+
+
+func _reset_step_points() -> void:
+	step_next_points = initial_step_next_points
+	print(step_next_points)
 	_beginning = step_next_points[0]
 	step_next_points.remove_at(0)
 
@@ -102,4 +134,12 @@ func _start_moving() -> void:
 func _rotate_spikeball(delta: float) -> void:
 	if _movement_blocked:
 		return
-	spikeball.rotation_degrees += 600 * delta
+	spikeball.rotation_degrees += 600 * delta * _direction
+
+
+func _calculate_direction(old_pos: Vector2, new_pos: Vector2) -> void:
+	var motion_vector = new_pos - old_pos
+	motion_vector = motion_vector.rotated(rotation)
+	var dir_to_set = signi(motion_vector.x)
+	if dir_to_set != 0:
+		_direction = dir_to_set
--- objects/centipede/centipede_draw_area.gd
+++ objects/centipede/centipede_draw_area.gd
@@ -7,8 +7,9 @@ func _process(delta: float) -> void:
 	queue_redraw()
 
 func _draw() -> void:
+	var par = $".."
 	if !Engine.is_editor_hint(): return
-	if !$"..".get(&"trigger_area_enabled"): return
+	if !par.get(&"trigger_area_draw_enabled") || par.trigger_type != 0: return
 	var trigger_area: Rect2 = $"..".trigger_area
 	draw_set_transform(-global_position, -global_rotation, Vector2.ONE / global_scale)
 	draw_rect(
