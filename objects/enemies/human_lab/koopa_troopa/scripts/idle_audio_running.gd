0c7c00644a15e1e87ca6f130429b5e7b3d21652c
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-07-03 01:31:38 +0900

    fix: running coder clones shouting out of screen

--- objects/enemies/human_lab/koopa_troopa/scripts/idle_audio_running.gd
+++ objects/enemies/human_lab/koopa_troopa/scripts/idle_audio_running.gd
@@ -1,11 +1,18 @@
 extends Timer
 
+@export var parent: NodePath = ^".."
+
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $"../AudioStreamPlayer2D"
+@onready var parent_node: CanvasItem = get_node(parent)
 
 func _ready() -> void:
 	await get_tree().create_timer(1.5, false, false, true).timeout
 	start()
 	timeout.connect(_on_timeout)
+
+
+func _physics_process(delta: float) -> void:
+	paused = !Thunder.view.is_getting_closer(parent_node, 64)
 
 
 func _on_timeout() -> void:
