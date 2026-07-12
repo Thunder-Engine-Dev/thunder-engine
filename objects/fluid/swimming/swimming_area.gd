8a83c3a61f5ba15b62759ea9baa17233ec0f559a
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-07-12 16:24:42 +0900

    feat: #water_body group for water splashes; jumping cheeps now create water splashes on impact with water

--- objects/fluid/swimming/swimming_area.gd
+++ objects/fluid/swimming/swimming_area.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
 const WATER_SPRAY: PackedScene = preload("res://engine/objects/effects/sprays/water_spray.tscn")
+
+@export var can_swim: bool = true
+
 var _is_ready: bool
+
+signal body_got_in_water_at(pos: Vector2)
+signal area_got_in_water_at(pos: Vector2)
 
 func _ready() -> void:
 	# Body in/out of water
@@ -9,13 +15,22 @@ func _ready() -> void:
 		func(body: Node2D) -> void:
 			if body.has_node("Underwater") && !body.get(&"is_underwater"):
 				var underwater: Node = body.get_node("Underwater")
-				underwater.in_water()
+				if can_swim:
+					underwater.in_water()
 				if !_is_ready:
 					return
 				if "warp" in body && body.warp != 0:
 					return
 				if "spray_offset" in underwater:
 					self._spray.call_deferred(body, underwater.spray_offset)
+			if body.is_in_group(&"#water_body") && body:
+				if body is Player && body.warp != Player.Warp.NONE:
+					return
+				if _is_ready:
+					self._spray.call_deferred(body, Vector2(0, 8))
+				if body.has_method(&"got_in_water"):
+					body.got_in_water()
+					body_got_in_water_at.emit(body.global_position)
 	)
 	body_exited.connect(
 		func(body: Node2D) -> void:
@@ -23,13 +38,28 @@ func _ready() -> void:
 				return
 			if body.has_node("Underwater") && body.get(&"is_underwater"):
 				var underwater: Node = body.get_node("Underwater")
-				underwater.out_of_water()
+				if can_swim:
+					underwater.out_of_water()
 				if !_is_ready:
 					return
 				if "warp" in body && body.warp != 0:
 					return
 				if "spray_offset" in underwater:
 					self._spray.call_deferred(body, underwater.spray_offset)
+			if body.is_in_group(&"#water_body"):
+				if body is Player && body.warp != Player.Warp.NONE:
+					return
+				if _is_ready:
+					self._spray.call_deferred(body, Vector2(0, 8))
+	)
+	area_entered.connect(
+		func(area: Area2D) -> void:
+			if area.is_in_group(&"#water_body"):
+				if _is_ready:
+					self._spray.call_deferred(area, Vector2(0, 8))
+				if area.has_method(&"got_in_water"):
+					area.got_in_water()
+					area_got_in_water_at.emit(area.global_position)
 	)
 	if Scenes.current_scene is Stage2D:
 		await Scenes.current_scene.stage_ready
