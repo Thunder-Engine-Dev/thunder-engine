088c43adda094ec8d4f531258cdbe597f310dfee
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-07-27 05:18:39 +0900

    fix: incorrect piranha plant speed compared to OG MF

--- objects/enemies/piranha_plants/piranha_in_pipe.gd
+++ objects/enemies/piranha_plants/piranha_in_pipe.gd
@@ -3,8 +3,8 @@ extends Node2D
 @export var stay_in_pipe_at_first: bool = true
 @export var range_in_pipe: float = 80.0
 @export var stay_in_interval: float = 1.0
-@export var stay_out_interval: float = 0.9
-@export var stretching_speed: float = 50.0
+@export var stay_out_interval: float = 1.0
+@export var stretching_speed: float = 41.67
 @export var stretching_length: float = 60.0
 @export var custom_vars: Dictionary
 @export var custom_script: Script
