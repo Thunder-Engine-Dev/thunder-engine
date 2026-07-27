50b0a3c32274c444973be45c7842737de3da5d6c
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-07-27 22:38:59 +0900

    upd: walk speed to 212.5, run speed to 365.625, red mar run speed to 416.25; update walk animation min speed again

--- objects/players/player_config.gd
+++ objects/players/player_config.gd
@@ -5,15 +5,15 @@ class_name PlayerConfig
 @export var walk_initial_speed: float = 50
 @export var walk_acceleration: float = 312.5
 @export var walk_deceleration: float = 312.5
-@export var walk_crouch_deceleration: float = 468.75
+@export var walk_crouch_deceleration: float = 625.0
 @export var walk_turning_acce: float = 1250
-@export var walk_max_walking_speed: float = 210
-@export var walk_max_running_speed: float = 360
+@export var walk_max_walking_speed: float = 212.5
+@export var walk_max_running_speed: float = 365.625
 @export var walk_running_grace_time: float = 0.05
 @export var walk_slow_walk_speed: float = 68.75
 @export_group("Underwater Walk", "underwater_walk_")
-@export var underwater_walk_max_walking_speed: float = 210
-@export var underwater_walk_max_running_speed: float = 210
+@export var underwater_walk_max_walking_speed: float = 212.5
+@export var underwater_walk_max_running_speed: float = 212.5
 @export_group("Jump", "jump_")
 @export var jump_speed: float = 700
 @export var jump_buff_static: float = 1000
@@ -36,8 +36,8 @@ class_name PlayerConfig
 @export var stuck_recovery_speed: float = 60
 @export_group("Animation", "animation_")
 @export var animation_walking_speed: float = 1.5
-@export var animation_max_walking_speed: float = 6
-@export var animation_min_walking_speed: float = 1.5
+@export var animation_max_walking_speed: float = 3.5
+@export var animation_min_walking_speed: float = 1.2
 @export_group("Sound", "sound_")
 @export var sound_jump: Array = [preload("res://engine/objects/players/prefabs/sounds/jump.wav")]
 @export var sound_swim: Array = [preload("res://engine/objects/players/prefabs/sounds/swim.wav")]
