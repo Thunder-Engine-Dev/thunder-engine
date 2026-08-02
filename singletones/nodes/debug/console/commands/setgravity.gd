a34d7b613300e691548c536b2e1e6f871752fd71
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-02 15:17:14 +0900

    feat(command): setgravity

---
+++ singletones/nodes/debug/console/commands/setgravity.gd
@@ -0,0 +1,16 @@
+extends Command
+
+static func register() -> Command:
+	return new().set_name("setgravity").add_param("value", TYPE_FLOAT) \
+		.set_description("Set global gravity of all bodies. Set to 0 for default gravity")
+
+func execute(args: Array) -> Command.ExecuteResult:
+	PhysicsServer2D.area_set_param(
+		GlobalViewport.vp.find_world_2d().space,
+		PhysicsServer2D.AREA_PARAM_GRAVITY, float(args[0])
+	)
+	PhysicsServer2D.area_set_param(
+		GlobalViewport.vp.find_world_2d().space,
+		PhysicsServer2D.AREA_PARAM_GRAVITY_IS_POINT, true
+	)
+	return Command.ExecuteResult.new("Success")
