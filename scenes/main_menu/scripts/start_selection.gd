227224f9bbaa53019ca65d65954ba17d0ca084ce
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-07 13:19:21 +0900

    replace buggy legacy transition code in start_selection.gd

--- scenes/main_menu/scripts/start_selection.gd
+++ scenes/main_menu/scripts/start_selection.gd
@@ -23,27 +23,7 @@ func _handle_select(mouse_input: bool = false) -> void:
 	Audio.play_1d_sound(_sfx, true, { "ignore_pause": true, "bus": "1D Sound" })
 	var sgr_path = ProjectSettings.get_setting("application/thunder_settings/save_game_room_path")
 	Data.technical_values.impulse_progress_continue = true
-
-	if SettingsManager.get_tweak("replace_circle_transitions_with_fades", false):
-		TransitionManager.accept_transition(
-			load("res://engine/components/transitions/crossfade_transition/crossfade_transition.tscn")
-				.instantiate()
-				.with_scene(sgr_path)
-		)
-		return
-
-	TransitionManager.accept_transition(
-		load("res://engine/components/transitions/circle_transition/circle_transition.tscn")
-			.instantiate()
-			.with_speeds(0.02, -0.1)
+	
+	Scenes.goto_scene_with_transition(sgr_path, func(t):
+		t.with_speeds(0.02, -0.1)
 	)
-
-	TransitionManager.transition_middle.connect(func():
-		TransitionManager.current_transition.paused = true
-		Scenes.goto_scene(sgr_path)
-		Scenes.scene_changed.connect(func(_current_scene):
-			TransitionManager.current_transition.on(Thunder._current_player, false, true)
-			if !Thunder._current_player:
-				TransitionManager.current_transition.paused = false
-		, CONNECT_ONE_SHOT)
-	, CONNECT_ONE_SHOT)
