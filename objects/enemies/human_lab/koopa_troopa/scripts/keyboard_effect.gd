7b4074301554ba52d696f6b1b85d33b051193090
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-14 14:18:01 +0900

    fix(min quality): spiny clone wasn't rotating; fix keyboard effect on min quality

--- objects/enemies/human_lab/koopa_troopa/scripts/keyboard_effect.gd
+++ objects/enemies/human_lab/koopa_troopa/scripts/keyboard_effect.gd
@@ -1,9 +1,22 @@
 extends Node2D
 
 const KEYBOARD_EFFECT = preload("res://engine/objects/enemies/human_lab/koopa_troopa/keyboard_effect.tscn")
+## Vector2.INF means default speed
+@export var set_speed_min_quality: Vector2 = Vector2(0, -600)
+## Vector2.INF means default speed
+@export var set_speed_other_quality: Vector2 = Vector2.INF
 
 func create() -> void:
 	var keyboard = KEYBOARD_EFFECT.instantiate()
+	var quality = SettingsManager.get_quality()
+	if set_speed_other_quality != Vector2.INF && quality != SettingsManager.QUALITY.MIN:
+		keyboard.default_speed_x_min = set_speed_other_quality.x
+		keyboard.default_speed_x_max = set_speed_other_quality.x
+		keyboard.speed = set_speed_other_quality
+	elif set_speed_min_quality != Vector2.INF && quality == SettingsManager.QUALITY.MIN:
+		keyboard.default_speed_x_min = set_speed_min_quality.x
+		keyboard.default_speed_x_max = set_speed_min_quality.x
+		keyboard.speed = set_speed_min_quality
 	Scenes.current_scene.add_child(keyboard)
 	keyboard.position = global_position
 	keyboard.reset_physics_interpolation()
