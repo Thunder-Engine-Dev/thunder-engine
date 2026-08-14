45589c5b8d2ef56179b568491e7aa84aef601258
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-14 14:17:07 +0900

    feat: big clouds face object

--- tilesets/rockland/cloud_face_anim.gd
+++ tilesets/rockland/cloud_face_anim.gd
@@ -2,9 +2,22 @@ extends AnimatedSprite2D
 
 func _ready() -> void:
 	_timer()
+	animation_finished.connect(_on_animation_finished)
 
 func _timer() -> void:
 	await get_tree().create_timer(randf_range(0.5, 3), false).timeout
 	
-	play("default")
+	if sprite_frames.has_animation(&"grin") && randi_range(1, 3) == 2 && !is_playing():
+		play(&"grin")
+		speed_scale = 0.3
+	else:
+		speed_scale = 1.0
+		play("default")
 	_timer()
+
+
+func _on_animation_finished() -> void:
+	if animation == &"grin":
+		animation = &"default"
+		frame = 0
+		stop()
