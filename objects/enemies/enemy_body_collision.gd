33edbcbf386e61d904c7529d32b4c0115164e0b4
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-09-24 06:09:50 +0900

    feat(enemy body): export var to disable antistuck with enemies

--- objects/enemies/enemy_body_collision.gd
+++ objects/enemies/enemy_body_collision.gd
@@ -4,6 +4,10 @@ extends Area2D
 @export var solid: bool = true
 @export var turn_back: bool = true
 @export var enable_offscreen: bool = true
+## If [code]true[/code], colliding with another enemy adds it to the ignore list for a fraction of a second,
+## to prevent from getting stuck when one is above another.[br]If you have many enemies close to each other,
+## you may have to disable this.
+@export var prevent_from_getting_stuck: bool = true
 
 var _prev_collided_areas = {}
 
@@ -12,7 +16,9 @@ func _ready() -> void:
 	area_entered.connect(
 		func(area: Area2D) -> void:
 			if area.get_script() != get_script() || area.owner == owner: return
-			if owner is GravityBody2D && area.solid && turn_back && !(area.get_instance_id() in _prev_collided_areas):
+			if owner is GravityBody2D && area.solid && turn_back && (
+				(prevent_from_getting_stuck && !area.get_instance_id() in _prev_collided_areas) || !prevent_from_getting_stuck
+			):
 				owner.turn_x()
 			
 			_prev_collided_areas[area.get_instance_id()] = 0
@@ -23,6 +29,9 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	if !prevent_from_getting_stuck:
+		return
+	
 	for area in _prev_collided_areas:
 		var instance = instance_from_id(area)
 		if !is_instance_valid(instance) || !overlaps_area(instance):
